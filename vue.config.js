3b34623cdb855fc57a22f0aaebcdc3549daee274
詹伟超 <weichao.zhan@kangcenet.com>
2018-12-07 15:18:06 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -24,34 +24,74 @@ module.exports = {
     configureWebpack: config => {
         // 依赖代码分离，避免 vendor 过大
         config.optimization = {
+            runtimeChunk: 'single',
             splitChunks: {
                 name: true,
                 chunks: 'all',
+                minSize: 10000,
+                maxInitialRequests: 6, // 首页最大并行下载数
                 cacheGroups: {
-                    vue: {
+                    vueBase: {
                         test: (module) => {
                             return /vue/.test(module.context);
                         },
                         name: 'vue',
+                        priority: 3,
+                        reuseExistingChunk: true,
+                    },
+                    zrender: {
+                        test: (module) => {
+                            return /zrender/.test(module.context);
+                        },
+                        name: 'zrender',
+                        priority: 3,
+                        reuseExistingChunk: true,
+                    },
+                    base: {
+                        test: (module) => {
+                            return /core-js|async-validator/.test(module.context);
+                        },
+                        name: 'base',
+                        priority: 3,
+                        reuseExistingChunk: true,
                     },
                     elementui: {
                         test: (module) => {
                             return /element-ui/.test(module.context);
                         },
                         name: 'elementui',
+                        priority: 3,
+                        reuseExistingChunk: true,
+                    },
+                    echarts: {
+                        test: (module) => {
+                            return /echarts/.test(module.context);
+                        },
+                        name: 'echarts',
+                        priority: 3,
+                        reuseExistingChunk: true,
+                    },
+                    axios: {
+                        test: (module) => {
+                            return /axios/.test(module.context);
+                        },
+                        name: 'axios',
+                        priority: 2,
+                        reuseExistingChunk: true,
                     },
                     moment: {
                         test: (module) => {
                             return /moment/.test(module.context);
                         },
                         name: 'moment',
+                        priority: 2,
+                        reuseExistingChunk: true,
                     },
-                    commons: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendors',
-                        priority: -1,
-                        // reuseExistingChunk: true,
-                    },
+                    // commons: {
+                    //     name: 'commons',
+                    //     minChunks: 2,
+                    //     priority: 0,
+                    // },
                 },
             }
         }
